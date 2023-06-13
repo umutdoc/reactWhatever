a4be298d6d695761b0d9ccf07a187cd9a14c35de
Umut Doruk Çağlayan <umutdorukcag@hotmail.com>
2023-06-13 18:47:44 +0300

    added youtube embed

--- src/Buttons.jsx
+++ src/Buttons.jsx
@@ -3,6 +3,21 @@ import "./Buttons.css";
 
 export default function Buttons() {
   const [banana, setBanana] = React.useState(132);
+  const [singer, setSinger] = React.useState(
+    "https://www.youtube.com/embed/jNQXAC9IVRw"
+    //https://www.youtube.com/watch?v=jNQXAC9IVRw&ab_channel=jawed
+  );
+  const urlHere = React.useRef();
+  function newSong() {
+    let raw = urlHere.current.value;
+    let encode = raw.indexOf("watch?v=");
+    let end = raw.indexOf("&");
+    encode = encode + 8;
+    let result = raw.substring(encode, end);
+    setSinger(`https://www.youtube.com/embed/${result}`);
+    console.log(result);
+  }
+
   function bananaShrink() {
     let b = banana;
     let p = b;
@@ -29,6 +44,17 @@ export default function Buttons() {
   return (
     <>
       <div className="container">
+        <div className="oppa-div">
+          <button onClick={newSong}>AA</button>
+          <iframe width="420" height="315" src={singer}></iframe>
+          <textarea
+            name="singer"
+            id="thesinger"
+            cols="1"
+            rows="1"
+            ref={urlHere}
+          ></textarea>
+        </div>
         <div className="banana-div">
           <p>Use these buttons to set the banana size (very important)</p>
           <img
