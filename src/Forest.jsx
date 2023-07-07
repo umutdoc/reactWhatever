0689898dd92de284def3275590817275d7157e68
Umut Doruk Çağlayan <umutdorukcag@hotmail.com>
2023-07-07 19:58:49 +0300

    ball

--- src/Forest.jsx
+++ src/Forest.jsx
@@ -3,7 +3,10 @@ import "./Forest.css";
 export default function Forest() {
   return (
     <>
-      <p>hi</p>
+      <p className="main-div">hi</p>
+      <div className="main-div">
+        <div className="ball" id="ball1"></div>
+      </div>
     </>
   );
 }
