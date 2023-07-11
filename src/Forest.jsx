cd8d8a69ff23672ee9187e60b031639bee9e6a05
Umut Doruk Çağlayan <umutdorukcag@hotmail.com>
2023-07-11 19:56:22 +0300

    added animations

--- src/Forest.jsx
+++ src/Forest.jsx
@@ -13,8 +13,17 @@ export default function Forest() {
       <div className="main-div">
         <div className="ball" id="ball1"></div>
       </div>
-      <div className="main-div">
+      <div className="main-div last">
         <div className="ball2" id="ball2"></div>
+      </div>
+      <div className="main-div last">
+        <div className="ball" id="ball3"></div>
+      </div>
+      <div className="main-div last">
+        <div className="ball" id="ball3"></div>
+      </div>
+      <div className="main-div last">
+        <div className="ballrnbw" id="ball4"></div>
       </div>
     </>
   );
