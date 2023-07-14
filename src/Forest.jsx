65ec0cb4becde6a6e61ee50bf85b041485a03c63
Umut Doruk Çağlayan <umutdorukcag@hotmail.com>
2023-07-14 23:22:17 +0300

    removed hazardous animation & div

--- src/Forest.jsx
+++ src/Forest.jsx
@@ -4,32 +4,31 @@ export default function Forest() {
   return (
     <>
       <p className="main-div">hi</p>
-      <div className="main-div">
-        <div className="ball" id="ball1"></div>
-      </div>
-      <div className="main-div">
-        <div className="ball" id="ball1"></div>
-      </div>
-      <div className="main-div">
-        <div className="ball" id="ball1"></div>
-      </div>
-      <div className="main-div last">
-        <div className="ball2" id="ball2"></div>
-      </div>
-      <div className="main-div last">
-        <div className="ball" id="ball3"></div>
-      </div>
-      <div className="main-div last">
-        <div className="ball" id="ball3"></div>
-      </div>
-      <div className="main-div last">
-        <div className="ballrnbw" id="ball4"></div>
-      </div>
-      <div className="main-div last">
-        <div className="ball" id="ball3"></div>
-      </div>
-      <div className="main-div last">
-        <div className="ball" id="ball5"></div>
+      <div>
+        <div className="main-div">
+          <div className="ball" id="ball1"></div>
+        </div>
+        <div className="main-div">
+          <div className="ball" id="ball1"></div>
+        </div>
+        <div className="main-div">
+          <div className="ball" id="ball1"></div>
+        </div>
+        <div className="main-div last">
+          <div className="ball2" id="ball2"></div>
+        </div>
+        <div className="main-div last">
+          <div className="ball" id="ball3"></div>
+        </div>
+        <div className="main-div last">
+          <div className="ball" id="ball3"></div>
+        </div>
+        <div className="main-div last">
+          <div className="ballrnbw" id="ball4"></div>
+        </div>
+        <div className="main-div last">
+          <div className="ball" id="ball3"></div>
+        </div>
       </div>
     </>
   );
